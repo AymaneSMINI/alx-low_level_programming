ac6016a4662f95553fe4c6a246f1479754eda281
aymanesmini <aymane.smini@gmail.com>
2022-09-09 08:48:30 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <sdlib.h>
+#include <stdio.h>
+/**
+ * main - file
+ * Description : alphabet
+ * Return : 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	for (alpha = 'a'; alpha <= 'z' ; alpha++)
+		putchar(alpha);
+	return (0);
+}
