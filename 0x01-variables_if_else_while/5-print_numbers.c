4951993bf530a59a8e16972c129bf8a73271ba6c
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-09-09 20:49:57 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,7 +9,7 @@ int main(void)
 {
 char number;
 
-for (number = '0'; number <= '9'; number++)
- putchar(number);
- return (0);
+ for (number = '0'; number <= '9'; number++)
+  putchar(number);
+  return (0);
  }
