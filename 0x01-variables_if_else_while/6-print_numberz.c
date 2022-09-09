830db6333ae7040e722917ec5a095fa7ba54a6b4
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-09-09 23:52:41 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	int digit;
 
-	for (digit = 0; digit < 10; digit++)
-		putchar('0' + digit);
+	for (digit = 0; digit < 9; digit++)
+		putchar(digit);
 	putchar('\n');
 	return (0);
 }
