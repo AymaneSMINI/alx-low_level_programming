6bc86fb01d1f3750e5729e7bfd817b3bafa3e6e2
aymanesmini <aymane.smini@gmail.com>
2022-09-09 13:42:57 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
 /**
 * main - main file
-* print alphabt
+* print digit
 * Return: 0 (Success)
 */
 int main(void)
 {
 	int digit;
 
-	for (digit=0; digit < 10; digit++)
+	for (digit = 0; digit < 10; digit++)
 		putchar('0' + digit);
 	putchar('\n');
 	return (0);
