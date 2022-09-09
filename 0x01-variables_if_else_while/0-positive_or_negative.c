90aacc8479d9ffb4001e269f06cf585f312e0ab2
aymanesmini <aymane.smini@gmail.com>
2022-09-09 07:49:08 -0700

    update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/*
+/**
  * main - main file
- * negative positive
+ *
+ * Description : 'negative positive'
+ * 
  * Return: 0  (Success)
  */
 int main(void)
