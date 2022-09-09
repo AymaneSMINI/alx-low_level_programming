3908d8c417dc8abd240d9d36c62ad437ca08f778
aymanesmini <aymane.smini@gmail.com>
2022-09-09 16:04:05 -0700

    smmile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main file
+ * Description: Smile in the mirror
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	char alpha;
+
+	for (alpha =  'z'; alpha <= 'a'; alpha--)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
