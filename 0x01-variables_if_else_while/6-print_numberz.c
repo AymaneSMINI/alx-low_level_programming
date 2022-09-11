3d63ce3d63eb81e16649cd42af066c2e900c3305
aymanesmini <aymane.smini@gmail.com>
2022-09-11 03:36:11 -0700

    update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - main file
-* print digit
-* Return: 0 (Success)
+*main - main file
+*print digit
+*Return: 0 (Success)
 */
 int main(void)
-{
-	int digit;
+{	int digit;
 	
 	for (digit = '0'; digit <= '9'; digit++)
 		putchar(digit);
