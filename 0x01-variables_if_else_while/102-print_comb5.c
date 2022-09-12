f9fb982e4fece47e7090c5d32be865bf50ea3450
aymanesmini <aymane.smini@gmail.com>
2022-09-12 13:08:48 -0700

    print 102 combin

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	int i,j,k,l;
+	int i, j, k, l;
 
 	for (i = '0'; i <= '9'; i++)
 	{
@@ -15,7 +15,7 @@ int main(void)
 		{
 			for (k = i; k <= '9'; k++)
 			{
-				for (l=j+1; l <= '9'; l++)
+				for (l = j + 1; l <= '9'; l++)
 				{
 					putchar(i);
 					putchar(j);
