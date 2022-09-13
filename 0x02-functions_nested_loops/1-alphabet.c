603bef2263d9ba8ba394ce7c3a825bd85c8cd9e0
aymanesmini <aymane.smini@gmail.com>
2022-09-13 01:43:16 -0700

    alphabet function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,5 +5,14 @@
  * Description: "Print _putchar"
  * Return: 0 Always (Success)
  */
+void print_alphabet(void)
+{
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+}
 int main(void)
 {
+	print_alphabet();
+	return (0);
+}
