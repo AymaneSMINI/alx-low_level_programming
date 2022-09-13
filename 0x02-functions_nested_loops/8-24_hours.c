0bfadeb2211ee82c63d6862f7e23b56824a3e5a9
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-09-13 18:58:53 +0100

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+void jack_bauer(void)
+{
+    char i, j, k, l, m;
+    m = '9';
+
+    for (i = '0'; i <= '2'; i++)
+    {
+        if (i == '2')
+            m = '3';
+        for (j = '0'; j <= m; j++)
+        {
+            for (k = '0'; k <= '5'; k++)
+            {
+                for (l = '0'; l <= '9'; l++)
+                {
+                    _putchar(i);
+                    _putchar(j);
+                    _putchar(':');
+                    _putchar(k);
+                    _putchar(l);
+                    _putchar('\n');
+                }
+            }
+        }
+    }
+}
+int main(void)
+{
+    jack_bauer();
+    return (0);
+}
