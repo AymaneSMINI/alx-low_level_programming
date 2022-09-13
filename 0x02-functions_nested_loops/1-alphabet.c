22a4e08c7ad122f38c87f4623d2b6fa1a249ff06
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-09-13 10:20:16 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 /**
  * main - file
  * Description: "Print _putchar"
  * Return: 0 Always (Success)
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 void print_alphabet(void)
 {
 	int alpha;
