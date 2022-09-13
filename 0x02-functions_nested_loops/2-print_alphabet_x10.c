1377d4c53ecb214d213fb55ae3a60e96e08be0b8
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-09-13 09:54:46 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,25 @@
 #include <stdlib.h>
 /**
  * main - file
- * Description: "Print _putchar"
+ * Description: "Print alphabet x 10"
  * Return: 0 Always (Success)
  */
+void print_alphabet_x10(void)
+{
+    int alpha;
+    int i;
+    
+    for (i = 0; i < 10; i++)
+    {
+    	for (alpha = 'a'; alpha <= 'z'; alpha++)
+    	{
+    	    putchar(alpha);
+    	}
+    	putchar('\n');
+	}
+}
 int main(void)
 {
+	print_alphabet_x10();
+	return (0);
+}
