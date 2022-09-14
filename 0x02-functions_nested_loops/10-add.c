8b61a9b4e4be8270ece6cff147eee11499994ae0
aymanesmini <aymane.smini@gmail.com>
2022-09-14 16:11:44 -0700

    uodate add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *add - sum.
- *@a: int argument
- *@b: int argument
- *Return: value.
+ * add - sum.
+ * @a: int argument
+ * @b: int argument
+ * Return: value.
  */
 int add(int a, int b)
 {
