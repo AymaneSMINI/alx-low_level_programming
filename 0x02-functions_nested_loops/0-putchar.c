73408556e949a0f139367cee89d124b6ea230c5e
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-09-14 01:32:04 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *main - file
  *Description: "Print _putchar"
@@ -12,7 +11,7 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 	{
-	    _putchar(word[i]);
+		_putchar(putchar[i]);
 	}
 	_putchar('\n');
 	return (0);
