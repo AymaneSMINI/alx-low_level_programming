093a284dc08c4ab6247fb15aeb001bf9f7fd58a0
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-09-14 11:45:08 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,36 +1,15 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <ctype.h>
+#include "main.h"
 
 /**
- * main - check the code.
+ * _isalpha - check if alpha.
  *
- * Return: Always 0.
+ * Return: 1 if is alpha 0 if not.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 int _isalpha(int c)
 {
     if (isalpha(c) > 0)
         return (1);
     else 
         return (0);
-}
-int main(void)
-{
-    int r;
-
-   r = _isalpha('H');
-    _putchar(r + '0');
-    r = _isalpha('o');
-    _putchar(r + '0');
-    r = _isalpha(108);
-    _putchar(r + '0');
-    r = _isalpha(';');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
 }
