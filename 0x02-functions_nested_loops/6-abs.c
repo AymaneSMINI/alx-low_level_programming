a7dbfb85bd2f446cf5bd880cd8a847335c14c2a0
aymanesmini <aymane.smini@gmail.com>
2022-09-14 04:24:07 -0700

    update abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _abs - absolute value.
- *@n : int argument 
+ *@n : int argument
  * Return: positive value.
  */
 int _abs(int n)
 {
-	if (n >= 0) 
+	if (n >= 0)
 	{
 		return (n);
 	}
