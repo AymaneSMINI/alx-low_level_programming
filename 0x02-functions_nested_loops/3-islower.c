382afbbaa832c2049cd40d5e7a0801b42ff87936
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-09-14 01:39:55 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,34 +1,13 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-
+#include "main.h"
 /**
- * main - check the code.
- *
+ * print_alphabet_x10 - void function
+ * islower
  * Return: Always 0.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 int _islower(int c)
 {
-    if (islower(c) > 0)
-        return (1);
-    else 
-        return (0);
-}
-int main(void)
-{
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+ 	if (islower(c) > 0)
+        	return (1);
+    	else 
+        	return (0);
 }
