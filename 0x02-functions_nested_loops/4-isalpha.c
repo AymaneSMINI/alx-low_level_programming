c261a6bed43057fc98a161ef8281f097fa9d837f
aymanesmini <aymane.smini@gmail.com>
2022-09-14 03:47:43 -0700

    update is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,13 @@
 
 /**
  * _isalpha - check if alpha.
- *
+ * @c: int argument
  * Return: 1 if is alpha 0 if not.
  */
 int _isalpha(int c)
 {
-    if (isalpha(c) > 0)
-        return (1);
-    else 
-        return (0);
+	if (isalpha(c) > 0)
+		return (1);
+	else
+		return (0);
 }
