4c49fce3da8d982dd1b3e478b5fc68d0190b001f
aymanesmini <aymane.smini@gmail.com>
2022-09-14 16:28:23 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function sum
+ * sum
+ * @a: int argument
+ * @b: int argument
+ * Return; value
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
