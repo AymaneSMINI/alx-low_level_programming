eb8f679e3af7125b9a82df44253690ccfd87873e
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-09-14 01:03:49 +0100

    Create 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: nothing.
+ */
+
+void print_long(int digit)
+{
+    if (digit != 0)
+    {
+        print_long(digit/10);
+        _putchar('0' + digit % 10);
+    }
+}
+void times_table(void)
+{
+    int i, j;
+    for (i=0; i <= 9; i++)
+    {
+        for (j=0; j <= 9; j++)
+        {
+            if (j != 0 && i*j<10)
+            {
+                _putchar(',');
+                _putchar(' ');
+                _putchar(' ');
+            }
+            else if (i*j >=10)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+
+            if (i*j == 0 )
+                _putchar('0');
+            else
+                print_long(i*j);
+
+        }
+        _putchar('\n');
+    }
+}
