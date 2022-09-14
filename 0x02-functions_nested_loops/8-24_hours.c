0a763d1563b6a939e0df5aa923a3ce1092f9c21f
aymanesmini <aymane.smini@gmail.com>
2022-09-14 13:35:06 -0700

    update hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,32 @@
 #include "main.h"
 /**
- * main - check the code.
+ * jack_bauer - check the code.
  *
  * Return: Always 0.
  */
 void jack_bauer(void)
 {
-    char i, j, k, l, m;
-    m = '9';
+	char i, j, k, l, m;
+	m = '9';
 
-    for (i = '0'; i <= '2'; i++)
-    {
-        if (i == '2')
-            m = '3';
-        for (j = '0'; j <= m; j++)
-        {
-            for (k = '0'; k <= '5'; k++)
-            {
-                for (l = '0'; l <= '9'; l++)
-                {
-                    _putchar(i);
-                    _putchar(j);
-                    _putchar(':');
-                    _putchar(k);
-                    _putchar(l);
-                    _putchar('\n');
-                }
-            }
-        }
+	for (i = '0'; i <= '2'; i++)
+	{
+		if (i == '2')
+			m = '3';
+		for (j = '0'; j <= m; j++)
+		{
+			for (k = '0'; k <= '5'; k++)
+			{
+				for (l = '0'; l <= '9'; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
     }
 }
