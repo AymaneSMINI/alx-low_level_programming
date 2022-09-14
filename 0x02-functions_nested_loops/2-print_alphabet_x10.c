93cde55355bffda38cc4aa4d283d8355a304f6c9
aymanesmini <aymane.smini@gmail.com>
2022-09-14 03:30:02 -0700

    update  alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,14 @@
 void print_alphabet_x10(void)
 {
 	int alpha;
-    	int i;
-    
-    	for (i = 0; i < 10; i++)
-    	{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
-    		{
-    	 	   _putchar(alpha);
+		{
+			_putchar(alpha);
 		}
-    		_putchar('\n');
+		_putchar('\n');
 	}
 }
