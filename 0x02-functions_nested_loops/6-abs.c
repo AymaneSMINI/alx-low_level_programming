4fbace291aa7ae882657f118c1cf9fff33364b67
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-09-14 11:56:49 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - check the code.
+ * _abs - absolute value.
  *@n : int argument 
  * Return: positive value.
  */
 int _abs(int n)
 {
     if (n >= 0) 
-    {   
+    {
         return (n);
     }
     else
