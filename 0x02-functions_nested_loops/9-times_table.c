7f70924d46e48e54f772bddd70b99ae30a93384f
aymanesmini <aymane.smini@gmail.com>
2022-09-14 13:48:21 -0700

    update times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,38 +7,36 @@
 
 void print_long(int digit)
 {
-    if (digit != 0)
-    {
-        print_long(digit/10);
-        _putchar('0' + digit % 10);
-    }
+	if (digit != 0)
+	{
+		print_long(digit/10);
+		_putchar('0' + digit % 10);
+	}
 }
 void times_table(void)
 {
-    int i, j;
-    
-    for (i=0; i <= 9; i++)
-    {
-        for (j=0; j <= 9; j++)
-        {
-            if (j != 0 && i*j<10)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-            }
-            else if (i*j >=10)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
+	int i, j;
 
-            if (i*j == 0 )
-                _putchar('0');
-            else
-                print_long(i*j);
-
-        }
-        _putchar('\n');
-    }
+	for (i=0; i <= 9; i++)
+	{
+		for (j=0; j <= 9; j++)
+		{
+			if (j != 0 && i*j<10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (i*j >=10)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (i*j == 0 )
+				_putchar('0');
+			else
+				print_long(i*j);
+		}
+		_putchar('\n');
+	}
 }
