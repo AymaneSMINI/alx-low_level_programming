f7c6b832001e17e76730b6547344a5a690c6cfee
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-09-15 15:12:04 +0100

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,1 +1,19 @@
-#
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - main file
+ * Description :'negative positive'
+ * @n : int argument
+ * Return: 0 (Success)
+ */
+void main(int n)
+{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
