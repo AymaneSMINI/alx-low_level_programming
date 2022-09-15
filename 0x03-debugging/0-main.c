420653daa47fef45fc93529ecc34f7ac2766b5bc
aymanesmini <aymane.smini@gmail.com>
2022-09-15 06:53:03 -0700

    first main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/** main - test function positive negative
+ * Return : 0
+ */
+
+int main(void)
+{
+	int i = 98;
+
+	positive_or_negative(i);
+	positive_or_negative(0);
+	
+	return (0);
+}
