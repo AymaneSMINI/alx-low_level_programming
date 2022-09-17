04fdad112cbf813e2fe719db9c819033bfb88fd2
aymanesmini <aymane.smini@gmail.com>
2022-09-17 12:48:39 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - function
+ * @n : int argument
+ * Return: print line
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		putchar('_');
+	}
+		_putchar('\n');
+}
