fb6e2e0236f1dcdadc9799dc35f68786be774f6f
aymanesmini <aymane.smini@gmail.com>
2022-09-17 14:17:19 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * fizz_buzz - function
+ * Return: print
+ */
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (i % 5 == 0)
+		{
+
+		}
+		else if (i % 5 && i % 3)
+		{
+
+		}
+		else
+		{
+
+		}
+	}
+		_putchar('\n');
+}
