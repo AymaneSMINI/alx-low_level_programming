bdd4987c1d8c2f3ad1b925c72a76e282beb7656f
aymanesmini <aymane.smini@gmail.com>
2022-09-19 15:23:47 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev -  function
+ * @s: char pointer
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = strlen(s) - 1;
+	while (s[i] >= 0 )
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
