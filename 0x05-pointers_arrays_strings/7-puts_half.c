0bfe03f03a3e258770273ddc49a2166dcc4bb2e9
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-09-20 00:30:39 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,16 @@
 void puts_half(char *str)
 {
 	int start;
+	int len;
 
 	len = strlen(str);
 	if (len % 2 == 0)
-		start = len - ((len - 1) / 2)
+		start = len - ((len - 1) / 2);
 	else
 		start = len - (len / 2)
 	while (str[start] != '\0')
 	{
-		_putchar(str[i]);
+		_putchar(str[start]);
 		start++;
 	}
 	_putchar('\n');
