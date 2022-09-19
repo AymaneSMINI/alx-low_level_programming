4cc1aac3625ee7af870e505ff5efc6c4f30a9406
aymanesmini <aymane.smini@gmail.com>
2022-09-19 16:39:58 -0700

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_array -  function
+ * @a: int pointer
+ * @n: int argument
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	while (a[i] != '\0')
+	{
+		_putchar(a[i]);
+		if (i != (n - 1)) 
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		i++;
+	}
+	_putchar('\n');
+}
