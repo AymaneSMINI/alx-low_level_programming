5450d8002e849de1d85670358be971e5b752f9a4
aymanesmini <aymane.smini@gmail.com>
2022-09-19 16:16:02 -0700

    put 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 -  function
+ * @str: char pointer
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 0 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
