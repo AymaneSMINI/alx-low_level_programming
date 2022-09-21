120d1f90d302f189c5699abe630aa9689ae7d037
aymanesmini <aymane.smini@gmail.com>
2022-09-21 11:53:25 -0700

    first push

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcat - function
+ * @dest: char pointer
+ * @src: char pointer
+ * Return: char pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
