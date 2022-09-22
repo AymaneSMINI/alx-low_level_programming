01e145c2b5ac2f0e588f06f332053927cc6c3a0a
aymanesmini <aymane.smini@gmail.com>
2022-09-22 12:14:51 -0700

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,8 +15,8 @@ char *cap_string(char *str)
 			str[i] = str[i] - 32;
 		if (str[i] == '\t' || str[i] == '\n' || str[i] == ' ' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
 		{
-			if (str[i+1] < 123 && str[i+1] > 96)
-				str[i+1] = str[i+1] - 32;
+			if (str[i + 1] < 123 && str[i + 1] > 96)
+				str[i + 1] = str[i + 1] - 32;
 		}
 		i++;
 	}
