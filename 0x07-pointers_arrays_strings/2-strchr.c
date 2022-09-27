edd0270a287ceb14b0c2a262bdceae2d97cc4628
aymanesmini <aymane.smini@gmail.com>
2022-09-27 11:54:23 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - function
+ * @s: char pointer
+ * @c: char argument
+ * Return: char pointer
+ */
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+	return (s);
+}
