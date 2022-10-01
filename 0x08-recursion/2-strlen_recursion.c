8f58a466ba9f0b0a06d2d18d7aa9d11ec1bb0b98
aymanesmini <aymane.smini@gmail.com>
2022-10-01 11:52:04 -0700

    update

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function
+ * @s: char pointer
+ * Return: integer
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (_strlen_recursion(s++) + 1);
+	}
+return;
+}
