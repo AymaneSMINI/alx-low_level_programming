b35bbcd0e04932286840c43c15e30636b9073970
aymanesmini <aymane.smini@gmail.com>
2022-10-04 17:25:23 -0700

    butterfly

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,10 +11,10 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *array;
-	
+
 	if (size == 0)
 		return (NULL);
-	array = malloc(size * (sizeof(char)));
+	array = (char*) malloc(size * (sizeof(char)));
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
