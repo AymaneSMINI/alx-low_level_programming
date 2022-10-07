2ee985d5767648c281485a55b490fcb976faa695
aymanesmini <aymane.smini@gmail.com>
2022-10-07 15:10:28 -0700

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - matrix
+ * @width: integer parameter
+ * @height: integer
+ * @Return: integer matrix
+ */
+int **alloc_grid(int width, int height)
+{
+	int **matrix;
+	int i;
+	int j;
+
+	if ( width <= 0 || height <= 0)
+		return (NULL);	
+	matrix = malloc( height * sizeof(int));
+	if (matrix == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+		matrix[i] = malloc(width * sizeof(int));
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			matrix[i][j] = 0;
+		}
+	}
+	return (matrix);
+}
