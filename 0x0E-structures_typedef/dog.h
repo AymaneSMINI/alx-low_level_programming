afc1631922170f0538bc7b0d729d57ff6e362941
aymanesmini <aymane.smini@gmail.com>
2022-10-11 12:16:35 -0700

    Dogs do speak, but only to those who know how to listen

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef _DOG_H
+#define _DOG_H
+
 /**
  * struct dog - define new type dog
  * @name: name of dog (string pointer)
@@ -17,3 +20,5 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif
