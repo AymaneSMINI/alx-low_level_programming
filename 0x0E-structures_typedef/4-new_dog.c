2382c753922beac407dad32eccfc6275ff92bbe9
aymanesmini <aymane.smini@gmail.com>
2022-10-11 12:30:52 -0700

    Dogs do speak, but only to those who know how to listen

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,16 +1,23 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * new_dog - initialize dog
  * @name: string pointer
  * @age: float argument
  * @owner: string pointer
- * Return: 
+ * Return: dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *proprietor;
+	char *title;
 
+	if (name == NULL)
+		return (NULL);
+	proprietor = owner;
+	title = name;
 	d = malloc(sizeof(dog_t));
 	if (d== NULL)
 		return (NULL);
