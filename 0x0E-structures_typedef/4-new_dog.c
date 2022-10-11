dcea12276d7581794630096b658b3ad59741ac62
aymanesmini <aymane.smini@gmail.com>
2022-10-11 12:33:40 -0700

    Dogs do speak, but only to those who know how to listen

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,16 +11,15 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	char *proprietor;
-	char *title;
 
 	if (name == NULL)
 		return (NULL);
-	proprietor = owner;
-	title = name;
 	d = malloc(sizeof(dog_t));
 	if (d== NULL)
+	{
+		free (d);
 		return (NULL);
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
