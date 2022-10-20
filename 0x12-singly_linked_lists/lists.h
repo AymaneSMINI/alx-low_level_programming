df0a4f6af7ee63cf37eb4d48b640c5c09881a137
aymanesmini <aymane.smini@gmail.com>
2022-10-20 13:35:38 -0700

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -21,4 +22,5 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
