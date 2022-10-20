99c144cf90c764e32298ece62626b851335e2969
aymanesmini <aymane.smini@gmail.com>
2022-10-20 13:05:13 -0700

    length of list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_list - the function print a list
+ * @h: header format list_t
+ * Return: number of node
+ */
+size_t list_len(const list_t *h)
+{
+	list_t *tmp;
+	size_t count = 0;
+
+	for (tmp = (list_t *) h; tmp != NULL; tmp = tmp->next)
+	{
+		count++;
+	}
+	return (count);
+}
