42ad5d5572d669d6c5c8904c3d0da56dc3fc6652
aymanesmini <aymane.smini@gmail.com>
2022-10-20 13:46:36 -0700

    add in the end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_node_end - function
+ * @head: pointer
+ * @str: char const
+ * Return: head
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *tmp;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		for (tmp = *head; tmp->next; tmp = tmp->next);
+		tmp->next = new;
+	}
+	return (*head);
+}
+
