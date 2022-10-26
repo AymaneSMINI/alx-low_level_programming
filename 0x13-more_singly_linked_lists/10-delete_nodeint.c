e96fd91cb24c6e106ca27213e509a6e2f699b61b
aymanesmini <aymane.smini@gmail.com>
2022-10-26 12:15:08 -0700

    delete function

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - function
+ * @head: pointer
+ * @index: integer
+ * Return: 1 True 0 false
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *tmp, *current;
+
+	current = *head;
+	if (index == 0)
+	{
+		if (current == NULL)
+			return (-1);
+		tmp = current;
+		current = current->next;
+		free(tmp);
+	}
+	else
+	{
+		for (i = 0; i < (index - 1) && current != NULL; i++)
+			current = current->next;
+		if ((current == NULL || current->next == NULL) && i != 0)
+			return (-1);
+		tmp = current->next;
+		current->next = tmp->next;
+		free(tmp);
+	}
+	return (1);
+}
