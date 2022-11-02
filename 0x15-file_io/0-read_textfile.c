9419885a8bb9244d65e724a33e429cd53dbb5a5e
aymanesmini <aymane.smini@gmail.com>
2022-11-02 10:07:04 -0700

    Read file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * read_textfile - function
+ * @filename: string pointer
+ * @letters: size_t
+ * Return: size_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	buf = malloc(letters);
+	
+	if (!buf)
+		return (0);
+	
+	f = open(filename, O_RDONLY);
+
+	if (f == -1)
+		return (0);
+
+	read(f, buf, letters);
+	printf("%s\n", buf);
+	close(f);
+	return (letters);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+
+#endif
